b6abbd080f1af6d755b1fad4d225d3dda65e1274
Yashraj Agawane <145097498+yashrajagawane@users.noreply.github.com>
2026-07-26 21:20:42 +0530

    Add imports for React Router and Axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
+
 import "@/App.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import axios from "axios";
+
 import { HOME } from "@/constants/testIds";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
